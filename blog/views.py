da3a464ed70fedd92a3b009049adca860dce2727
DFCMK <dkalnoky@gmail.com>
2024-03-02 18:24:43 +0000

    added for loop to show success message for User sign up

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts = [
-    {
-    'author': 'CoreyMS',
-    'title': 'Blog post 1',
-    'content': 'First post content',
-    'date_posted': 'August 27, 2018'
-    },
-    {
-    'author': 'Jane Dow',
-    'title': 'Blog post 2',
-    'content': 'Second post content',
-    'date_posted': 'August 28, 2018'
-    },
-]
-
-# Create your views here.
 def home(request):
-    context = { 'posts': posts }
+    context = { 'posts': Post.objects.all() }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
         return render(request, 'blog/about.html', {'title': 'About'})
-
