24b98289a68f1241596089f4205f98575bb6c09b
DFCMK <dkalnoky@gmail.com>
2024-03-04 12:45:58 +0000

    Added Profile class

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
