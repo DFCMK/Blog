21ebce722a9e4e2f49b4fd096c2cca5780d9b5d9
DFCMK <dkalnoky@gmail.com>
2024-03-04 12:46:36 +0000

    added profile function to handle profile image updates

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+from .forms import ProfileForm
+from .models import Profile
+
+
 
 
 def register(request):
@@ -10,7 +14,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your Account has been created! You are now able to log in')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -19,4 +23,19 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    try:
+        profile = Profile.objects.get(user=request.user)
+    except Profile.DoesNotExist:
+        profile = Profile.objects.create(user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+
+    context = {'profile': profile, 'form':form}
+    print(f"Updated profile image URL: {profile.profile_image.url}")
+    return render(request, 'users/profile.html', context)
