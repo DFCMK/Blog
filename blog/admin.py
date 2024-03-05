26002d5f56e2d8740e6d5ba6f167fc52e79cfde0
DFCMK <dkalnoky@gmail.com>
2024-03-05 18:16:27 +0000

    added style rules to main.css for profile section

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
-#@admin register(Post)
+#@admin.register(Post)
 #class PostAdmin(SummernoteModelAdmin):
 #
 #    list_display = ('title', 'slug', 'status')
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -9,3 +9,5 @@ def home(request):
 
 def about(request):
         return render(request, 'blog/about.html', {'title': 'About'})
+
+
