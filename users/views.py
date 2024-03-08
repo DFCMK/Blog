695e66a715858b3d8de8366e18af1fc789671511
DFCMK <dkalnoky@gmail.com>
2024-03-08 18:15:53 +0000

    moved context and return out of else statement

--- users/views.py
+++ users/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
-from .forms import ProfileUpdateForm
-from .forms import UserUpdateForm
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
 from .models import Profile
 
 
@@ -30,16 +28,17 @@ def profile(request):
         profile = Profile.objects.create(user=request.user)
 
     if request.method == 'POST':
-        user_form = UserUpdataForm(request.POST, instance=request.user)
+        user_form = UserUpdateForm(request.POST, instance=request.user)
         profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, f"Your profile has been updated!")
             return redirect('profile')
     else:
-        user_form = UserUpdateForm(instance=request.user)#
+        user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=profile)
         
-        context = {'profile': profile, 'user_form': user_form, 'profile_form': profile_form}
+    context = {'profile': profile, 'user_form': user_form, 'profile_form': profile_form}
         
-        return render(request, 'users/profile.html', context)
+    return render(request, 'users/profile.html', context)
