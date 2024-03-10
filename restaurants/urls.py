bc1f3d5d613b9a4c88c20caf2ae1105eba01dc91
DFCMK <dkalnoky@gmail.com>
2024-03-10 09:43:26 +0000

    added deleted urls to urlpatterns

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('profile/', user_views.profile, name='profile'),
+    path('delete/', user_views.delete_profile, name='delete'),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
