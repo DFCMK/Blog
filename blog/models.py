9102b407bab52674e1f95c107c681dea5171ce92
DFCMK <dkalnoky@gmail.com>
2024-03-12 14:12:50 +0000

    added slug, status and excerpt to Post model

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
 
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    featured_image = CloudinaryField('post_image', default='placeholder')
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+
 
     def __str__(self):
         return self.title
 
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
