df0c6595a271cb75abd899ddfd86bb10146b4f7c
DFCMK <dkalnoky@gmail.com>
2024-03-12 14:11:57 +0000

    created PostAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -3,14 +3,16 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
-#@admin.register(Post)
-#class PostAdmin(SummernoteModelAdmin):
-#
-#    list_display = ('title', 'slug', 'status')
-#    search_fields = ['title', 'content']
-#    list_filter = ('status', 'created_on')
-#    prepopulated_fields = {'slug': ('title',)}
-#    summernote_fields = ('content',)
+@admin.register(Post)# Based on CI walk threw project
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'date_posted')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
 
 # Register your models here
-admin.site.register(Post)
+# admin.site.register(Post)
