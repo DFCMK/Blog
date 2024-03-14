5834fc78ff10cb44ad768bd22c060f229ea45d51
DFCMK <dkalnoky@gmail.com>
2024-03-14 17:09:50 +0000

    changed post_detail path to get slug as URL

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, post_detail
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('<post:pk/', PostDetailView.as_view(), name="post_detail"),
+    path('<slug:slug>/', views.post_detail, name="post_detail"),
     path('about/', views.about, name='blog-about')
-
 ]
