e4745ddad71d2591391f1129a5b706c827f7c2f6
DFCMK <dkalnoky@gmail.com>
2024-03-14 17:15:45 +0000

    changed from class based view to function based view to get slug functionality

--- blog/views.py
+++ blog/views.py
@@ -15,16 +15,67 @@ class PostListView(ListView):
     ordering = ['-date_posted']
 
 
-# Tutorial video 10 - 11:20min
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
+def post_detail(request, slug):
+    """
+    Function-based view to display a specific post based on its slug and primary key.
+    """
 
-    def get_object(self, queryset=None):
-        slug = self.kwargs.get('slug')
-        if slug is not None:
-            return get_object_or_404(Post, slug=slug)
-        return super().get_object(queryset=queryset)
+    try:
+        # Attempt to retrieve the post object based on slug and pk
+        post = get_object_or_404(Post, slug=slug)
+    except Post.DoesNotExist:
+        # Handle the case where the post does not exist
+        return render(request, '404.html', status=404)  # Render a 404 page
+
+    context = {
+        'post': post,
+        'slug': slug,
+    }
+
+    return render(request, 'blog/post_detail.html', context)
+
+
+#def post_detail(request, slug):
+#    """
+#    Display an individual :model:`blog.Post`.
+#
+#    **Context**
+#
+#    ``post``
+#        An instance of :model:`blog.Post`.
+#
+#    **Template:**
+#
+#    :template:`blog/post_detail.html`
+#    """
+#    queryset = Post.objects.filter(status=1)
+#    post = get_object_or_404(queryset, slug=slug)
+#    comments = post.comments.all().order_by("-date_posted")
+#    comment_count = post.comments.filter(approved=True).count()
+#    if request.method == "POST":
+#        comment_form = CommentForm(data=request.POST)
+#        if comment_form.is_valid():
+#            comment = comment_form.save(commit=False)
+#            comment.author = request.user
+#            comment.post = post
+#            comment.save()
+#            messages.add_message(
+#               request, messages.SUCCESS,
+#                'Comment submitted and awaiting approval'
+#            )
+#    
+#    comment_form = CommentForm()
+#
+#    return render(
+#        request,
+#        "blog/post_detail.html",
+#        {
+#            "post": post,
+#            "comments": comments,
+#            "comment_count": comment_count,
+#            "comment_form": comment_form
+#        },
+#    )
 
 
 
