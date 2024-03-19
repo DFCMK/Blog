1799c04b46aecf85d07cce0ca9228afc01f787af
DFCMK <dkalnoky@gmail.com>
2024-03-19 22:34:26 +0000

    added Js function to handle new post form

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,67 @@
 function changeTitleColor() {
     const postDetailTitles = document.getElementsByClassName('post-title');
 
+    // Iterate over every single Post to change heading colors
     for (let i = 0; i < postDetailTitles.length; i++) {
         postDetailTitles[i].style.color = 'white';
     }
 }
 
 window.addEventListener('load', changeTitleColor);
+
+
+
+/* Update Blog Post
+document.addEventListener('DOMContentLoaded', function() {
+    const editButton = document.getElementById('edit-button');
+    const editForm = document.getElementById('edit-form');
+
+    editButton.addEventListener('click', function() {
+        editForm.style.display = 'block';
+        editButton.style.display = 'none';
+    });
+
+    const cancelButton = document.getElementById('cancel-button');
+    cancelButton.addEventListener('click', function() {
+        editForm.style.display = 'none';
+        editButton.style.display = 'block';
+    });
+});*/
+
+
+// Update Blog Post
+document.addEventListener('DOMContentLoaded', function() {
+    const editButton = document.getElementById('edit-button');
+    const editForm = document.getElementById('edit-form');
+    const cancelButton = document.getElementById('cancel-button');
+
+    if (editButton && editForm && cancelButton) {
+        editButton.addEventListener('click', function() {
+            editForm.style.display = 'block';
+            editButton.style.display = 'none';
+        });
+
+        cancelButton.addEventListener('click', function() {
+            editForm.style.display = 'none';
+            editButton.style.display = 'block';
+        });
+    }
+});
+
+// Create New Post
+document.addEventListener('DOMContentLoaded', function() {
+    // Show form when "New Post" button is clicked
+    document.getElementById('new-post-btn').addEventListener('click', function() {
+        document.getElementById('form-container').style.display = 'block';
+        document.getElementsByClassName('card-body').style.display = 'none'; // hide New Post container when form is opened
+        document.getElementById('page-title').style.color = 'white'; // THIS HAVE TO BE ADJUSTED TO CHANGE ALL HEADING COLORS WHEN FORM TRIGGERED!!!
+    });
+
+    // Change the color of the page title to white when the window is loaded
+    changeTitleColor();
+
+    // Change the color of the page title to white
+    function changeTitleColor() {
+        document.getElementById('page-title').style.color = 'white';// THIS ISN'T OPTIMAL NETHER, ADJUST IT TO CHANGE ALL HEADING COLORS WITHIN THE FORM!!!!
+    }
+});
