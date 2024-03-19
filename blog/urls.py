ad26831939127b7d73438299f335fc1469fb3fa8
DFCMK <dkalnoky@gmail.com>
2024-03-19 22:21:50 +0000

    replaced class based urls path with function based

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, post_detail
+from .views import PostListView, post_detail, create_new_post
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('<slug:slug>/', views.post_detail, name="post_detail"),
+    path('create', create_new_post, name="new_post"),
     path('about/', views.about, name='blog-about')
 ]
