2d5cbd2965ae56697049418a4113a21243f06762
DFCMK <dkalnoky@gmail.com>
2024-03-22 17:48:42 +0000

    added form to Create new Posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Post
 
+
 class CreateNewPostForm(forms.ModelForm):
     class Meta: 
         model = Post
-        fields = ['title', 'slug', 'content', 'excerpt']
+        fields = ['title', 'slug', 'content', 'excerpt', 'author', 'featured_image']
+
+    def save(self, commit=True):
+        instance = super(CreateNewPostForm, self).save(commit=False)
+        if commit:
+            instance.save()
+            if self.cleaned_data['featured_image']:
+                instance.featured_image = self.cleaned_data['featured_image']
+                instance.save()
+        return instance
+
