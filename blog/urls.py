a273a8eb7b18a63b9bd4974b54b176e165866007
DFCMK <dkalnoky@gmail.com>
2024-03-24 18:38:16 +0000

    added update_post to urlspatterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, post_detail, create_new_post
+from .views import PostListView, post_detail, create_new_post, update_post
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('<slug:slug>/', views.post_detail, name="post_detail"),
     path('create', create_new_post, name="new_post"),
+    path('update_post/<slug:slug>/', views.update_post, name='update_post'),
     path('about/', views.about, name='blog-about')
 ]
