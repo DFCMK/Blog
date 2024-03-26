3dcdd90553dff978d430a5c6b8ac0522e422ff35
DFCMK <dkalnoky@gmail.com>
2024-03-26 20:01:25 +0000

    added and modified CI post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import ListView
-from .forms import CreateNewPostForm, PostUpdateForm
+from .forms import CreateNewPostForm, PostUpdateForm, CommentForm
 from django.contrib import messages
 from .models import Post
 
@@ -19,24 +21,95 @@ class PostListView(ListView):
     paginate_by = 6
 
 
-# Based on simple.rocks: https://simpleit.rocks/python/django/generating-slugs-automatically-in-django-easy-solid-approaches/
+# Based on simple.rocks: https://simpleit.rocks/python/django/generating-slugs-automatically-in-django-easy-solid-approaches/ to create slugs
+# Based on djangocentral: https://djangocentral.com/creating-comments-system-with-django/ for implementing comments
+# Based on CI walk threw
+#def post_detail(request, slug):
+
+
+    #try:
+    #    queryset = Post.objects.filter(status=1)
+    #    post = get_object_or_404(Post, slug=slug)
+    #    comments = post.comments.all().order_by('-date_posted')
+    #    comments_count = post.comments.filter(approved=True).count()
+    #    comment_form = CommentForm()
+    #except Post.DoesNotExist:
+    #    return render(request, '404.html', status=404)
+    #post = get_object_or404(Post, slug=slug)
+    #comment_form = CommentForm()
+    # Handle comment form submission
+    #if request.method == 'POST':
+    #    comment_form = CommentForm(data=request.POST)
+    #    if comment_form.is_valid():
+    #        # Create Comment object but don't save to database yet
+    #        comment = comment_form.save(commit=False)
+    #        # Assign Logged in user as comment author
+    #        comment.author = request.user
+    #        # Assign the current post to the comment
+    #        comment.post = post
+    #        # Save the comment to the database
+    #        comment.save()
+    #        messages.add_message(request, messages.SUCCESS, 'Comment submitted and awaiting approval')
+    #        # Refresh the page to display the new comment
+    #        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+    # If it's not a POST request or the form is invalid, render the page as usual
+    #else:
+    #    comment_form = CommentForm()
+
+    #context = {
+    #    'post': post,
+    #    'slug': slug,
+    #    'comment_form': comment_form,
+    #}
+
+    #comments = post.comments.filter(approved=True)
+
+    #return render(request, 'blog/post_detail.html', context)
+
+
+# CI Walkthrew view
 def post_detail(request, slug):
     """
-    Function-based view to display a specific post based on its slug and primary key.
+    Display an individual :model:`blog.Post`.
+        
+    **Context**
+        
+    ``post``
+    An instance of :model:`blog.Post`.
+        
+    **Template:**
+        
+    :template:`blog/post_detail.html`
     """
 
-    try:
-        post = get_object_or_404(Post, slug=slug)
-       # post.generate_excerpt()
-    except Post.DoesNotExist:
-        return render(request, '404.html', status=404)
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.all().order_by("-date_posted")
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    
+    comment_form = CommentForm()
 
-    context = {
-        'post': post,
-        'slug': slug,
-    }
-
-    return render(request, 'blog/post_detail.html', context)
+    return render(
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
+        },
+    )
 
 
 # Based users/views.py profile
