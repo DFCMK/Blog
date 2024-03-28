b2eae1730394d588ad904cd1c40c0edf6b780c10
DFCMK <dkalnoky@gmail.com>
2024-03-28 20:43:34 +0000

    added edit and delete_comment views

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, post_detail, create_new_post, update_post, delete_post
+from .views import PostListView, post_detail, create_new_post, update_post, delete_post, edit_comment, delete_comment
 from . import views
 
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
     path('create', create_new_post, name="new_post"),
     path('update_post/<slug:slug>/', views.update_post, name='update_post'),
     path('delete_post/<slug:slug>/', views.delete_post, name='delete_post'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.edit_comment, name='edit_comment'), # CI Walk threw 
+    path('<slug:slug>/delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'), # CI Walk threw
     path('about/', views.about, name='blog-about')
 ]
