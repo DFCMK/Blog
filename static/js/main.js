ef6287af9a113edb25f636d52da6609498b886ee
DFCMK <dkalnoky@gmail.com>
2024-03-28 20:47:37 +0000

    moved Edit comment logic into seperate file (comments.js)

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("load static");
     const formContainer = document.getElementById('form-container');
     const postDetailTitles = document.getElementsByClassName('post-title');
     const newPostBtn = document.getElementById('new-post-btn');
@@ -9,6 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const editButton = document.getElementById('edit-button');
     const editFormContainer = document.getElementById('edit-form-container');
     const cancelButton = document.getElementById('cancel-button');
+
+    // Edit Comment Button
+    /*const editButtons = document.getElementsByClassName("edit-comment");
+    const commentText = document.getElementById("id_body");
+    const commentForm = document.getElementById("commentForm");
+    const submitButton = document.getElementById("submitButton");*/
 
     function changeTitleColor() {
 
@@ -75,4 +82,39 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#deletePostModal .modal-footer .btn-secondary').addEventListener('click', function() {
         document.getElementById('deletePostModal').style.display = 'none';
     });
+
+    // Edit Comment
+    document.querySelectorAll('.btn-edit-comment').forEach(button => {
+        button.addEventListener('click', function() {
+            const commentId = this.getAttribute('data-comment-id');
+            const commentBody = document.querySelector(`#comment${commentId} p`).textContent;
+            document.querySelector('#id_body').value = commentBody.trim();
+            document.querySelector('#comment-id').value = commentId;
+            document.querySelector('#edit-comment-form').style.display = 'block';
+        });
+    });
+    
+    document.querySelectorAll('.btn-delete-comment').forEach(button => {
+        button.addEventListener('click', function() {
+            const commentId = this.getAttribute('data-comment-id');
+            document.querySelector('#comment-id').value = commentId;
+            document.querySelector('#delete-comment-form').style.display = 'block';
+        });
+    });
+
+    // Edit Comment / Toggle Edit button
+   /* for (let button of editButtons) {
+        button.addEventListener("click", (e) => {
+            // Get the comment ID and content
+            let commentId = e.target.getAttribute("comment_id");
+            let commentContent = document.getElementById(`comment${commentId}`).innerText;
+            
+            // Update the form fields and button text
+            commentText.value = commentContent;
+            submitButton.innerText = "Update";
+            
+            // Update the form action to include comment ID
+            commentForm.setAttribute("action", `edit_comment/${commentId}`);
+        });
+    }*/
 });
