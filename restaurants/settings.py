5a7bdbb8124532fb9816ad52f5ff52a83fe61c02
DFCMK <dkalnoky@gmail.com>
2024-03-30 13:38:05 +0000

    fix broken Database urls

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -111,32 +111,17 @@ WSGI_APPLICATION = 'restaurants.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-# Initialize DATABASES
-DATABASES = {}
-
-# Retrieve the DATABASE_URL environment variable
-DATABASE_URL = os.environ.get("SECRET_KEY")
 
 # Set a fallback database configuration if DATABASE_URL is not set
-if not DATABASE_URL:
+if not 'DATABASE_URL' in os.environ:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 else:
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
-
-
-#DATABASES = {
-#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#}
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        }
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.herokuapp.com"
