49d672273e4c9dff04cc1eb7e4999b2cf8720acb
DFCMK <dkalnoky@gmail.com>
2024-03-31 14:58:10 +0100

    deleted unused urls path

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import PostListView, post_detail, create_new_post, update_post, delete_post, edit_comment, delete_comment
 from . import views
 
+
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('<slug:slug>/', views.post_detail, name="post_detail"),
@@ -10,5 +11,5 @@ urlpatterns = [
     path('delete_post/<slug:slug>/', views.delete_post, name='delete_post'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.edit_comment, name='edit_comment'), # CI Walk threw 
     path('<slug:slug>/delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'), # CI Walk threw
-    path('about/', views.about, name='blog-about')
+    path('about/', views.about, name='blog-about'),
 ]
