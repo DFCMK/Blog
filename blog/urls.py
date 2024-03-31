799535d3aec82a81e8e8731f6fcc55d1cfe35f12
DFCMK <dkalnoky@gmail.com>
2024-03-31 18:53:37 +0100

    moved about view below PostListView to fix 404 error

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from .views import PostListView, post_detail, create_new_post, update_post, delete_post, edit_comment, delete_comment
+from .views import PostListView, post_detail, create_new_post, update_post, delete_post, edit_comment, delete_comment, about
 from . import views
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
     path('<slug:slug>/', views.post_detail, name="post_detail"),
     path('create', create_new_post, name="new_post"),
     path('update_post/<slug:slug>/', views.update_post, name='update_post'),
     path('delete_post/<slug:slug>/', views.delete_post, name='delete_post'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.edit_comment, name='edit_comment'), # CI Walk threw 
     path('<slug:slug>/delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'), # CI Walk threw
-    path('about/', views.about, name='blog-about'),
 ]
