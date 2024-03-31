cc616cde5950d9e19633e226027e2e59974f05e9
DFCMK <dkalnoky@gmail.com>
2024-03-31 15:02:15 +0100

    added static import to fix static file loading error

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -19,6 +19,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
