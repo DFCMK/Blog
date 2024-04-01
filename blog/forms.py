955e14c09173db8d2fad365ed53b55e3ec94134b
DFCMK <dkalnoky@gmail.com>
2024-04-01 17:08:39 +0100

    deleted author field from CreateNewPostForm

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from .models import Post, Comment
 class CreateNewPostForm(forms.ModelForm):
     class Meta: 
         model = Post
-        fields = ['title', 'slug', 'content', 'excerpt', 'author', 'featured_image']
+        fields = ['title', 'slug', 'content', 'excerpt', 'featured_image']
 
     def save(self, commit=True):
         instance = super(CreateNewPostForm, self).save(commit=False)
