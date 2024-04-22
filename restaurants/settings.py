6b1d78ceff9efa3400d2095c0152c956bf3b6588
DFCMK <dkalnoky@gmail.com>
2024-04-22 21:28:12 +0100

    added jazzmin config for darkmode

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -65,18 +66,116 @@ CLOUDINARY_STORAGE = {
     'CLOUDINARY_API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
-if os.environ.get('ENVIRONMENT') == 'production':
-    JAZZMIN_SETTINGS = {
-        "topmenu_links": [
-        {"name": "Back To Website",  "url": "/", "permissions": ["auth.view_user"]},
+# Based on Jazzmin Documentation: https://django-jazzmin.readthedocs.io/configuration/
+#JAZZMIN_SETTINGS = {
+#    "topmenu_links": [
+#        {"name": "Back To Website",  "url": "/", "permissions": ["auth.view_user"], "visible_on":["all"]},
+#        ],
+#    }
+#else:
+#    JAZZMIN_SETTINGS = {
+#        "topmenu_links": [
+#        {"name": "Back To Website",  "url": "https://django-restaurant-blog-d6ddec41b70e.herokuapp.com", "permissions": ["auth.view_user"], "visible_on":["all"]},
+#    ],
+#    }
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Restaurant Reviews",
+    "site_header": "Restaurant Review",
+    "site_brand": "Restaurant Reviews",
+    "site_icon": "images/favicon.png",
+    # Add your own branding here
+    "site_logo": None,
+    "welcome_sign": "Welcome to the Restaurant reviews",
+    # Copyright on the footer
+    "copyright": "Restaurant Reviews",
+    "user_avatar": None,
+    ############
+    # Top Menu #
+    ############
+    # Links to put along the top menu
+    "topmenu_links": [
+        # Url that gets reversed (Permissions can be added)
+        {"name": "Back To website", "url": "/", "permissions": ["auth.view_user"], "visible_on":["all"]},
+        # model admin to link to (Permissions checked against model)
+        {"model": "auth.User"},
     ],
-    }
-else:
-    JAZZMIN_SETTINGS = {
-        "topmenu_links": [
-        {"name": "Back To Website",  "url": "https://django-restaurant-blog-d6ddec41b70e.herokuapp.com", "permissions": ["auth.view_user"]},
-    ],
-    }
+    #############
+    # Side Menu #
+    #############
+    # Whether to display the side menu
+    "show_sidebar": True,
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "users.User": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "admin.LogEntry": "fas fa-file",
+    },
+    # # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-arrow-circle-right",
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    "related_modal_active": False,
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    # Uncomment this line once you create the bootstrap-dark.css file
+    "custom_css": "css/bootstrap-dark.css",
+    "custom_js": None,
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": False,
+    ###############
+    # Change view #
+    ###############
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    "changeform_format_overrides": {
+        "auth.user": "collapsible",
+        "auth.group": "vertical_tabs",
+    },
+}
+
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-success",
+    "accent": "accent-teal",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-info",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cyborg",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success",
+    },
+}
 
 
 
@@ -94,6 +193,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware', # Recently Added, testing required it, but not sure yet if really necessery
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -103,6 +203,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'restaurants.urls'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage' # recently added not sure yet if really necessery
 
 TEMPLATES = [
     {
@@ -137,6 +238,10 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
         }
+
+# Added for Testing
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Cloudinary storage set up 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
