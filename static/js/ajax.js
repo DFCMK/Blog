7f81fb26691de5197502aa8f77a0600d3494946b
DFCMK <dkalnoky@gmail.com>
2024-05-03 19:44:20 +0100

    deleted outcommented unused code snippet

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -30,38 +30,3 @@ $(document).ready(function () {
         });
     });
 });
-
-// post-id Approach
-/*$(document).ready(function () {
-    $(document).on('click', '.thumbaction', function (e) {
-        e.preventDefault();
-        console.log("Thumb button clicked");
-        // Get post ID from the closest parent container with data-id attribute
-        var postid = $(this).closest('.post-container').data('id'); 
-        var button = $(this).attr("value");
-        $.ajax({
-            type: 'POST',
-            url: `/thumbs/${postid}`,
-            data: {
-                postid: postid,
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
-                action: 'thumbs',
-                button: button,
-            },
-            success: function (json) {
-                if (json.length < 1 || json == undefined) {
-                }
-                $("#up").text(json['up']);
-                $("#down").text(json['down']);
-                $("svg").removeClass("thumb-active");
-                if (json['remove'] == 'none') {
-                    $("#" + button).removeClass("thumb-active");
-                } else {
-                    $("#" + button).addClass("thumb-active");
-                }
-            },
-            error: function (xhr, errmsg, err) {
-            }
-        });
-    });
-});*/
